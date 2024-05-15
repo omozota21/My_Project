2fdafcb8190b6b6d4449f3c998e210c7bd46d5aa
omozota21 <omozota21@iesrioarba.es>
2024-05-15 08:44:19 +0200

    .

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
 //importamos los componentes
 import CompShowBlogs from './blog/ShowBlogs';
 import CompCreateBlog from './blog/CreateBlog';
@@ -12,6 +17,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+ 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />        
